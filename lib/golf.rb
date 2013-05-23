43dc8b8e5c0e940fe692e836cd29bb5c6961ff20
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-05-23 19:04:57 -0400

    Working on total score

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,26 +1,97 @@
 require 'csv'
 
-class HoleLayout < Array
+#=====================================================================================#
+#================================= Global Methods ====================================#
+#=====================================================================================#
 
+def parse_file(file)
+  temp = CSV.parse(File.read(File.join(File.dirname(__FILE__), file)))
+end
+
+def sum
+  sum = 0
+  self.each { |num| sum += num }
+  return sum
+end
+
+#=====================================================================================#
+#================================ Class Definitions ==================================#
+#=====================================================================================#
+
+class HoleLayout < Array
   attr_accessor :par
 
   def load_file(file)
-    hole_pars = (CSV.parse(File.read(File.join(File.dirname(__FILE__), file)))).flatten
-    hole_pars.each do |par|
-      self.push(par.to_i)
-    end
-  end
-
-  def sum
-    sum = 0
-    self.each { |num| sum += num }
-    return sum
+    hole_pars = parse_file(file).flatten
+    hole_pars.each { |par| self.push(par.to_i) }
   end
 
   def par
     sum
   end
-
 end
 
+#=====================================================================================#
 
+class ScoreCard < Hash
+  attr_accessor :course, :score_term
+
+  def initialize
+    @score_term = ["par", "bogey", "ace", "eagle", "birdie"]
+  end
+
+
+  def load_file(file)
+    array = parse_file(file)
+    make_individual_scorecards(array)
+    scores_to_i
+  end
+
+  def make_individual_scorecards(array)
+    array.each { |sub_array| self[sub_array[0]] = sub_array[1..-1] }
+  end
+
+  def scores_to_i
+    self.each do |key, array|
+      numerical_array = []
+      array.each do |num|
+        numerical_array.push(num.to_i)
+      end
+      self[key] = numerical_array
+    end
+  end
+
+  def load_course(file)
+    @course = HoleLayout.new()
+    @course.load_file(file)
+  end
+
+  def deviation(hole, player)
+    index = hole - 1
+    if self[player][index] == 1
+      score = -3
+    elsif
+      score = self.course[index] - self[player][index]
+      return score
+    end
+  end
+
+  def score(deviation)
+    if deviation > 1
+      times = deviation - 1
+      return "#{times}x bogey"
+    else
+      return @score_term[deviation]
+    end
+  end
+
+  def total_score(player)
+    total_score = 0
+    (1..18).each do |hole|
+      hole_score = deviation(hole, player)
+      total_score += hole_score
+    end
+    return total_score
+  end
+
+end
--- spec/golf_spec.rb
+++ spec/golf_spec.rb
@@ -2,19 +2,94 @@ require 'rspec'
 require_relative '../lib/golf.rb'
 
 describe HoleLayout do
+  context "Loading a course layout" do
+    let(:correct_layout) { HoleLayout.new().push(3, 4, 5, 3, 4, 5, 4, 4, 5, 3, 4, 5, 3, 3, 4, 5, 3, 3) }
+    let(:empty_layout) { HoleLayout.new() }
+    let(:example_course_file) { 'example_pars.csv' }
 
-  let(:correct_layout) { HoleLayout.new().push(3, 4, 5, 3, 4, 5, 4, 4, 5, 3, 4, 5, 3, 3, 4, 5, 3, 3) }
+    it "calculates the par for the course" do
+      expect(correct_layout.par).to eql(70)
+    end
+
+    it "loads a CSV that contains the par per hole for a particular course" do
+      empty_layout.load_file(example_course_file)
+      expect(empty_layout.par).to eql(70)
+    end
+  end
+end
+
+describe ScoreCard do
+
+  let(:empty_card) { ScoreCard.new() }
+  let(:example_scores_file) { 'example_scores.csv' }
+  let(:score_card) { ScoreCard.new().load_file('example_scores.csv') }
   let(:empty_layout) { HoleLayout.new() }
-  let(:example_file) { 'example_file.csv' }
+  let(:example_course_file) { 'example_pars.csv' }
 
-  it "calculates the par for the course" do
-    expect(correct_layout.par).to eql(70)
+  it "loads a CSV of player scores" do
+    empty_card.load_file(example_scores_file)
+    expect(empty_card).to be_kind_of(Hash)
+    expect(empty_card['Woods']).to be_kind_of(Array)
+    expect(empty_card['Woods']).to eql([1, 4, 5, 3, 4, 5, 4, 4, 5, 3, 4, 5, 3, 3, 4, 5, 3, 3])
   end
 
-  it "loads a CSV" do
-    empty_layout.load_file(example_file)
-    expect(empty_layout.par).to eql(70)
+  it "loads a Hole Layout" do
+    score_card.load_course(example_course_file)
+    expect(score_card.course).to eql([3, 4, 5, 3, 4, 5, 4, 4, 5, 3, 4, 5, 3, 3, 4, 5, 3, 3])
   end
 
+  it "can tell par of an individual hole" do
+    score_card.load_course(example_course_file)
+    expect(score_card.course[0]). to eql(3)
+  end
+
+  it "loads all the scores of an individual player" do
+    expect(score_card['Woods'][0]).to eql(1)
+    expect(score_card['Woods'][1]).to eql(4)
+  end
+
+  it "tells a player's deviation from par for an individual hole" do
+    score_card.load_course(example_course_file)
+    expect(score_card.deviation(1, 'Woods')).to eql(-3)
+    expect(score_card.deviation(2, 'Woods')).to eql(0)
+  end
+
+  it "scores par for a deviation of 0" do
+    score_card.load_course(example_course_file)
+    expect(score_card.score(0)).to eql("par")
+  end
+
+  it "scores bogey for a deviation of 1" do
+    score_card.load_course(example_course_file)
+    expect(score_card.score(1)).to eql("bogey")
+  end
+
+  it "scores birdie for a deviation of -1" do
+    score_card.load_course(example_course_file)
+    expect(score_card.score(-1)).to eql("birdie")
+  end
+
+  it "scores eagle for a deviation of -2" do
+    score_card.load_course(example_course_file)
+    expect(score_card.score(-2)).to eql("eagle")
+  end
+
+  it "scores ace for a hole in one" do
+    score_card.load_course(example_course_file)
+    deviation = score_card.deviation(1, 'Woods')
+    expect(score_card.score(deviation)).to eql("ace")
+  end
+
+  it "calculates very large bogeys" do
+    score_card.load_course(example_course_file)
+    expect(score_card.score(15)).to eql("14x bogey")
+  end
+
+  it "calculates a player's total score" do
+    score_card.load_course(example_course_file)
+    expect(score_card.total_score('Woods')).to eql(-2)
+  end
 
 end
+
+
